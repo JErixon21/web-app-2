1580c1bf677dc78db619f31cbd18f12621969c4c
Jake Erickson <jake212e@gmail.com>
2025-02-20 03:00:48 +0000

    End of Class

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -5,4 +5,17 @@ class TacosController < ApplicationController
     # render :template => "tacos/index"
   end
 
+  def create
+    #   # start with a new Post
+    @post = Post.new
+    #   # assign user-entered form data to Company's columns
+    @post["author"] = params["author"]
+    @post["body"] = params["body"]
+    @post["image"] = params["image"]
+    #   # save Company row
+    @post.save
+    #   # redirect user
+    redirect_to "/posts"
+  end
+
 end
